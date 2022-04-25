70677850e62508264e839d057631dabc4123bc90
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-25 07:32:37 +0200

    fix issues

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,8 +1,10 @@
 const { connection } = require("src/connections");
 
-const queryPromise = ({ query }) => {
+const queryPromise = ({ query, values }) => {
+    values = values ? values : [];
+
     return new Promise((resolve, reject) => {
-        connection.query(query, (error, results) => {
+        connection.query(query, values, (error, results) => {
             if(error) {
                 return reject(error);
             }
--- src/pages/api/todos/index.js
+++ src/pages/api/todos/index.js
@@ -1,12 +1,30 @@
 const { queryPromise } = require("src/helpers/db")
+const { v4 } = require('uuid');
 
 const requestHandler = async (req, res) => {
     switch(req.method) {
         case "GET": {
             try {
-                const rows = await queryPromise({ query: "SELECT * FROM todos"});
+                const rows = await queryPromise({ query: "SELECT * FROM todos" });
                 res.send({ todos: rows })
             } catch(error) {
+                console.error(error)
+                res.status(500).json({ message: "Internal server error"});
+            }
+            break;
+        }
+        case "POST": {
+            try {
+                const { isActive, name } = req.body;
+
+                const rows = await queryPromise({ query: "SELECT * FROM todos"});
+                await queryPromise({ 
+                    query: `INSERT INTO todos (ID, name, isActive, position) VALUES (?, ?, ?, ?)`,
+                    values: [ v4(), name, isActive, rows.length + 1]
+                });
+                res.status(204).send()
+            } catch(error) {
+                console.log(error)
                 res.status(500).json({ message: "Internal server error"});
             }
             break;
